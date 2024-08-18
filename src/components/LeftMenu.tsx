f8b23e07aa6d76ae7a2858d93b59baccf9c9b273
Ismaeil Saberi <ismaeilsaberi@gmail.com>
2024-08-17 20:52:56 -0700

    another simple structural components before data base

--- src/components/LeftMenu.tsx
+++ src/components/LeftMenu.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
-// import ProfileCard from "./ProfileCard";
-// import Ad from "../Ad";
+import ProfileCard from "./ProfileCard";
+import Ad from "./Ad";
 
 const LeftMenu = ({ type }: { type: "home" | "profile" }) => {
   return (
     <div className="flex flex-col gap-6">
-      {/* {type === "home" && <ProfileCard />} */}
+      {type === "home" && <ProfileCard />}
       <div className="p-4 bg-white rounded-lg shadow-md text-sm text-gray-500 flex flex-col gap-2">
         <Link
           href="/"
@@ -88,7 +88,7 @@ const LeftMenu = ({ type }: { type: "home" | "profile" }) => {
           <span>Settings</span>
         </Link>
       </div>
-      {/* <Ad size="sm"/> */}
+      <Ad size="sm" />
     </div>
   );
 };
