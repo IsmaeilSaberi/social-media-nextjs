c3fc9a8f80c633235640dcee098c77862a6ac3d0
Ismaeil Saberi <ismaeilsaberi@gmail.com>
2024-08-19 04:55:28 -0700

    starting to use server actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,16 @@ const Homepage = () => {
   return (
     <div className="flex gap-6 pt-6">
       <div className="hidden xl:block w-[20%]">
-        <LeftMenu type="home" />
+        {/* <LeftMenu type="home" /> */}
       </div>
       <div className="w-full lg:w-[70%] xl:w-[50%]">
         <div className="flex flex-col gap-6">
-          <Stories />
+          {/* <Stories /> */}
           <AddPost />
-          <Feed />
+          {/* <Feed /> */}
         </div>
       </div>
-      <div className="hidden lg:block w-[30%]">
-        <RightMenu />
-      </div>
+      <div className="hidden lg:block w-[30%]">{/* <RightMenu /> */}</div>
     </div>
   );
 };
--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,45 +1,62 @@
-"use client";
+// "use client";
 
-import { useUser } from "@clerk/nextjs";
+import prisma from "@/lib/client";
+// import { useUser } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 // import { CldUploadWidget } from "next-cloudinary";
 import Image from "next/image";
-import { useState } from "react";
+// import { useState } from "react";
 // import AddPostButton from "./AddPostButton";
 // import { addPost } from "@/lib/actions";
 
 const AddPost = () => {
-  const { user, isLoaded } = useUser();
-  const [desc, setDesc] = useState("");
-  const [img, setImg] = useState<any>();
+  const { userId } = auth();
+  console.log(userId);
 
-  if (!isLoaded) {
-    return "Loading...";
-  }
+  // const { user, isLoaded } = useUser();
+  // const [desc, setDesc] = useState("");
+  // const [img, setImg] = useState<any>();
+
+  const testAction = async (formData: FormData) => {
+    // "use server";
+    // const desc = formData.get("desc") as string;
+    // try {
+    //   prisma.post.create({
+    //     data: { userId:, desc: desc },
+    //   });
+    // } catch (error) {
+    //   console.log(error);
+    // }
+  };
+
+  // if (!isLoaded) {
+  //   return "Loading...";
+  // }
 
   return (
     <div className="p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm">
       {/* AVATAR */}
-      <Image
+      {/* <Image
         src={user?.imageUrl || "/noAvatar.png"}
         alt=""
         width={48}
         height={48}
         className="w-12 h-12 object-cover rounded-full"
-      />
+      /> */}
       {/* POST */}
       <div className="flex-1">
         {/* TEXT INPUT */}
         <form
+          action={testAction}
           //   action={(formData) => addPost(formData, img?.secure_url || "")}
           className="flex gap-4"
         >
-          <textarea
+          {/* <textarea
             placeholder="What's on your mind?"
             className="flex-1 bg-slate-100 rounded-lg p-2"
             name="desc"
             onChange={(e) => setDesc(e.target.value)}
-          ></textarea>
+          ></textarea> */}
           <div className="">
             <Image
               src="/emoji.png"
@@ -48,6 +65,7 @@ const AddPost = () => {
               height={20}
               className="w-5 h-5 cursor-pointer self-end"
             />
+            <button>send</button>
             {/* <AddPostButton /> */}
           </div>
         </form>
